280144a18752e1db6d6d5aa285c6dee5082be1e0
yurii bilous <yurii.b@elogic.com.ua>
2024-05-22 20:01:39 +0300

    added project structure components tree

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+import { ChanelDetail, VideoDetail, SearchFeed, Navbar, Feed } from './components';
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Box sx={{backgroundColor: "#000"}}>
+        <Navbar />
+        <Routes>
+          <Route path='/' exact element={<Feed />}/>
+          <Route path='/video/:id' element={<VideoDetail />}/>
+          <Route path='/chanel/:id' element={<ChanelDetail />}/>
+          <Route path='/search/:searchTerm' element={<SearchFeed />}/>
+        </Routes>
+      </Box>
+    </BrowserRouter>
   )
 }
 
