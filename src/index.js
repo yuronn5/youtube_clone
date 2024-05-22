8df8aa654a31b629695bb8772847987de6596929
yurii bilous <yurii.b@elogic.com.ua>
2024-05-22 19:32:26 +0300

    added styles file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
-import ReactDom from "react-dom";
+import ReactDom from "react-dom/client";
 
 import App from "./App";
+import './index.css';
 
 const root = ReactDom.createRoot(document.getElementById('root'));
 
