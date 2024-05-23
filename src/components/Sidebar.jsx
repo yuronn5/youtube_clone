c3375209061d99a430107f167b1bdd8148320e36
yurii bilous <yurii.b@elogic.com.ua>
2024-05-23 12:57:58 +0300

    feed and sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,19 @@
+import { Stack } from "@mui/material";
+import { categories } from "../utils/constans";
+
+const selectedCategory = 'New';
+
+const Sidebar = () => {
+  return (
+    <Stack direction="row" sx={{ overflowY: 'auto', height: { sx: 'auto', md: '95%' }, flexDirection: { md: 'column' } }}>
+        {categories.map((category) => (
+            <button key={category.name} className="category-btn" style={{ background: category.name === selectedCategory && '#FC1503', color: '#fff' }}>
+                <span style={{ color: category.name === selectedCategory ? 'white' : 'red', marginRight: '15px' }} className="category-btn-icon">{category.icon}</span>
+                <span style={{ opacity: category.name === selectedCategory ? '1' : '0.8' }} className="category-btn-text">{category.name}</span>
+            </button>
+        ))}
+    </Stack>
+  )
+}
+
+export default Sidebar
