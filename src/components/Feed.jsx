4bbe40ffff48a39446811f481123bb11d829ded2
yurii bilous <yurii.b@elogic.com.ua>
2024-05-27 20:30:15 +0300

    videos component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
-import Sidebar from './Sidebar';
+import { Sidebar, Videos } from './';
+
 
 const Feed = () => {
   return (
@@ -11,6 +12,14 @@ const Feed = () => {
           <Typography className='copyright' variant='body2' sx={{ mt: 1.5, color: '#fff', }}>
             Copyright 2022
           </Typography>
+        </Box>
+
+        <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+          <Typography variant='h4' fontWeight='bold' mb={2} sx={{ color: '#fff' }}>
+            New <span style={{ color: '#f31503' }}>videos</span>
+          </Typography>
+
+          <Videos videos={[]} />
         </Box>
     </Stack>
   )
